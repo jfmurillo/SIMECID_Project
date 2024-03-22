6aa4eb519756128959699387b6eb7e331209deaa
Alison Padilla <apadillale@ucenfotec.ac.cr>
2024-03-21 23:50:03 -0400

    Creacion de AppointmentDTO

---
+++ DTO/Appointment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Appointment : BaseDTO
+    {
+        public int PatientId;
+
+        public int NurseId;
+
+        public int DoctorId;
+
+        public int ServiceId;
+
+        public int BranchId;
+
+        public int DiagnosticId;
+
+        public DateTime AppointmentDate;
+
+        public string motive;
+
+        public string status;
+
+    }
+}
