358f9879f534f97ad044d01da433b2f27b4b1986
Alison Padilla <apadillale@ucenfotec.ac.cr>
2024-03-26 16:33:27 -0400

    Codigo html y css del SideBar.

---
+++ WebApp/Pages/SideBarTemplate.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages
+{
+    public class SideBarTemplateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
