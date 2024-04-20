f6fb55fdb7c9bf5589367a59a0cee3351f260926
Sebastian <smongeq@ucenfotec.ac.cr>
2024-04-20 09:15:03 -0600

    Vistas de los roles

---
+++ WebApp/Pages/Nurse-Examination.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages
+{
+    public class Nurse_ExaminationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
