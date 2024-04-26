1b9da0c97f5eeed69f1b1e888471ee0233b4c394
jfmurillo <joseph.murillo1621@gmail.com>
2024-04-25 22:15:13 -0600

    Fixed bug

--- WebApp/wwwroot/js/login.js
+++ WebApp/wwwroot/js/login.js
@@ -24,7 +24,7 @@ function LoginController() {
             let lc = new LoginController();
             let urlParams = new URLSearchParams(window.location.search);
             email = urlParams.get("email")
-            lc.NewPasswordUpdate(); 
+            lc.NewPasswordUpdate();
         })
 
     }
@@ -35,7 +35,7 @@ function LoginController() {
 
         let loginData = {
             Email: email,
-            Password: password 
+            Password: password
         }
 
         serviceRoute = "Login/Authenticate"
@@ -44,10 +44,10 @@ function LoginController() {
             if (response.status == 200) {
                 console.log("Login successful");
                 this.ManageRol(email);
-                
+
             } else {
                 console.log(response)
-                console.log("Error during login:", response.message); 
+                console.log("Error during login:", response.message);
                 throw new Error("Error during login")
             }
         });
@@ -107,73 +107,74 @@ function LoginController() {
             }
         });
 
-    this.ValidateOTP = function (email, otp) {
-        if (email === "" || otp === "") {
-            return;
-        }
+        this.ValidateOTP = function (email, otp) {
+            if (email === "" || otp === "") {
+                return;
+            }
 
-        let data = {
-            email,
-            otp
-        }
+            let data = {
+                email,
+                otp
+            }
 
-        var srv = "RecoverPassword/CreateData"
-        ca.PostToAPI(srv, data, function (response) {
-            console.log("Creating data for database");
-            var serviceRoute = "RecoverPassword/VerifyOtp";
+            var srv = "RecoverPassword/CreateData"
+            ca.PostToAPI(srv, data, function (response) {
+                console.log("Creating data for database");
+                var serviceRoute = "RecoverPassword/VerifyOtp";
 
-            ca.GetToApi(serviceRoute, function (response) {
-                console.log(response);
-                var validOTP = false;
-                for (var i = 0; i < response.length; i++) {
-                    if (response[i].otp === otp && response[i].email === email) {
-                        validOTP = true;
-                        break;
+                ca.GetToApi(serviceRoute, function (response) {
+                    console.log(response);
+                    var validOTP = false;
+                    for (var i = 0; i < response.length; i++) {
+                        if (response[i].otp === otp && response[i].email === email) {
+                            validOTP = true;
+                            break;
+                        }
                     }
-                }
-                if (validOTP) {
-                    console.log("Valid OTP");
-                    setTimeout(function () {
-                        window.location.href = `/NewPassword?email=${data.email}`;
-                    }, 1000);
-                    
-                } else {
-                    console.log("Invalid OTP");
-                }
-            }, function (error) {
-                console.error("Error validating OTP:", error);
+                    if (validOTP) {
+                        console.log("Valid OTP");
+                        setTimeout(function () {
+                            window.location.href = `/NewPassword?email=${data.email}`;
+                        }, 1000);
+
+                    } else {
+                        console.log("Invalid OTP");
+                    }
+                }, function (error) {
+                    console.error("Error validating OTP:", error);
+                });
             });
-        });
-    };
-
-    this.NewPasswordUpdate = function () {
-        let newPassword = $("#new-pass").val();
-        let confirmPassword = $("#conf-pass").val();
-        let email;
-
-        if (newPassword !== confirmPassword) {
-            throw new Error("Passwords don't match")
-        }
-
-        let urlParams = new URLSearchParams(window.location.search);
-        email = urlParams.get("email");
-
-        let data = {
-            email: email,
-            newPassword: newPassword,
-            confirmPassword: confirmPassword
         };
 
-        let serviceRoute = "RecoverPassword/Update";
+        this.NewPasswordUpdate = function () {
+            let newPassword = $("#new-pass").val();
+            let confirmPassword = $("#conf-pass").val();
+            let email;
 
-        ca.PostToAPI(serviceRoute, data, function (response) {
-            console.log(response);
+            if (newPassword !== confirmPassword) {
+                throw new Error("Passwords don't match")
+            }
 
-            setTimeout(function () {
-                window.location.href = `/Login`;
-            }, 2000);
-        });
-    };
+            let urlParams = new URLSearchParams(window.location.search);
+            email = urlParams.get("email");
+
+            let data = {
+                email: email,
+                newPassword: newPassword,
+                confirmPassword: confirmPassword
+            };
+
+            let serviceRoute = "RecoverPassword/Update";
+
+            ca.PostToAPI(serviceRoute, data, function (response) {
+                console.log(response);
+
+                setTimeout(function () {
+                    window.location.href = `/Login`;
+                }, 2000);
+            });
+        };
+    }
 }
 
     $(document).ready(function () {
