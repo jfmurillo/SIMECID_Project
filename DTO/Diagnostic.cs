1f19e9cc4d12c84e9069380b4df491a93c1ed15b
Sebastian <smongeq@ucenfotec.ac.cr>
2024-04-27 09:29:56 -0600

    Cambios de medical report y creacion de diagnostic

---
+++ DTO/Diagnostic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Diagnostic : BaseDTO
+    {
+        public int PatientId { get; set; }
+
+        public string DiagnoseName { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Description { get; set; }
+
+
+    }
+}
---
+++ WebAPI/Controllers/DiagnosticController.cs
@@ -0,0 +1,81 @@
+﻿using DataAccess.CRUD;
+using DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class DiagnosticController : ControllerBase
+    {
+
+        [HttpPost]
+        [Route("Create")]
+        public ActionResult Create(Diagnostic diagnostic)
+        {
+            try
+            {
+                var dg = new DiagnosticCrudFactory();
+                dg.Create(diagnostic);
+                return Ok(diagnostic);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+
+        [HttpGet]
+        [Route("RetrieveAll")]
+        public ActionResult RetrieveAll()
+        {
+            try
+            {
+                var dg = new DiagnosticCrudFactory();
+                var lastDiagnostic = dg.RetrieveAll<Diagnostic>();
+                return Ok(lastDiagnostic);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("Update")]
+        public ActionResult Update(Diagnostic diagnostic)
+        {
+            try
+            {
+                var dg = new DiagnosticCrudFactory();
+                dg.Update(diagnostic);
+                return Ok("Diagnostic updated successfully");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("Delete")]
+        public ActionResult Delete(Diagnostic diagnostic)
+        {
+            try
+            {
+                var dg = new DiagnosticCrudFactory();
+                dg.Delete(diagnostic);
+                return Ok("Diagnostic deleted successfully");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+    }
+}
